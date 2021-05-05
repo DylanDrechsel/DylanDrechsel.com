b7f747f92996b90f02abd2a7c07381a448d126a1
DylanDrechsel <DrechselDylan@gmail.com>
2021-05-05 14:16:06 -0400

    Added character gifs to Dungeon Dive Component

--- src/Projects/DungeonDive/Info/MainInfo.js
+++ src/Projects/DungeonDive/Info/MainInfo.js
@@ -1,18 +1,40 @@
-import React from 'react';
-import { motion } from 'framer-motion'
+import React, { useEffect } from 'react';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
 const MainInfo = () => {
-    return (
-			<motion.div className="MainInfo">
-				<h1 style={{ fontSize: "32px" }}>
-                    <b>
+	const { ref, inView } = useInView();
+	const animation = useAnimation();
+
+	useEffect(() => {
+		if (inView) {
+			animation.start({
+				opacity: 1,
+				transition: {
+					duration: 1.5,
+				},
+			});
+		}
+
+		if (!inView) {
+			animation.start({
+				opacity: 0,
+			});
+		}
+	}, [inView]);
+	
+	return (
+		<motion.div ref={ref} animate={animation} className='MainInfo'>
+			<h1 style={{ fontSize: '32px' }}>
+				<b>
 					Dungeon Dive is a web app using React that is inspired by the early
-					<br />8-bit GameBoy games. Fight your way through the dungeon to reach the
+					<br />
+					8-bit GameBoy games. Fight your way through the dungeon to reach the
 					end and survive the many dangers it holds!
-                    </b>
-				</h1>
-			</motion.div>
-		);
+				</b>
+			</h1>
+		</motion.div>
+	);
 };
 
 export default MainInfo;
