8d68df2cccab7846aeb36e6d989273156c7b3b4d
Dylan Drechsel <drechseldylan@gmail.com>
2025-05-27 13:13:31 -0400

    Added utility classes for inline styling just for margin and padding so its easier to make quick changes to them

--- src/Pages/StartMenu/StartMenu.tsx
+++ src/Pages/StartMenu/StartMenu.tsx
@@ -2,8 +2,8 @@ import { useState, useEffect, type FC } from 'react';
 import './StartMenu.scss';
 import StaticAnimation from './StaticAnimation/StaticAnimation';
 import RollingCubeAnimation from './RollingCubeAnimation/RollingCubeAnimation';
-import PixelButtonFadeOut from '../../Common/Components/Buttons/PixelButtonFadeOut/PixelButtonFadeOut';
-import PixelButtonFadeIn from '../../Common/Components/Buttons/PixelButtonFadeIn/PixelButtonFadeIn';
+// import PixelButtonFadeOut from '../../Common/Components/Buttons/PixelButtonFadeOut/PixelButtonFadeOut';
+// import PixelButtonFadeIn from '../../Common/Components/Buttons/PixelButtonFadeIn/PixelButtonFadeIn';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface StartMenuProps {};
@@ -36,10 +36,21 @@ const StartMenu: FC<StartMenuProps> = () => {
     // Define the props for the cubes
     const desiredCubeProps = [
         { x: -250, y: 0, letter: 'D' },
-        { x: -125, y: 0, letter: 'Y' },
-        { x: 0, y: 0, letter: 'L' },
-        { x: 125, y: 0, letter: 'A' },
+        { x: -125, y: -25, letter: 'Y' },
+        { x: 0, y: -50, letter: 'L' },
+        { x: 125, y: -25, letter: 'A' },
         { x: 250, y: 0, letter: 'N' },
+    ];
+
+    const secondDesiredCubeProps = [
+        { x: -500, y: - 200, letter: 'D'},
+        { x: -375, y: - 200, letter: 'R'},
+        { x: -250, y: - 200, letter: 'E'},
+        { x: -125, y: - 200, letter: 'C'},
+        { x:  125, y: - 200, letter: 'H'},
+        { x:  250, y: - 200, letter: 'S'},
+        { x:  375, y: - 200, letter: 'E'},
+        { x:  500, y: - 200, letter: 'L'},
     ];
 
     return (
@@ -49,14 +60,16 @@ const StartMenu: FC<StartMenuProps> = () => {
         )}
 
         <div className={`start-menu-content ${showMainContent ? 'visible' : ''}`}>
-            <div className={'padding'} />
+            {/* <div className={'padding'} />
             <PixelButtonFadeOut color={'#ff5722'} title={'Hover Me'} width={180} height={60}/>
             <div className={'padding'} />
             <PixelButtonFadeIn color={'#03a9f4'} title={'Fade In'} width={180} height={60}/>
-            <div className={'padding'} />
-            {/* <h1>TEST FROM THE START MENU</h1> */}
+            <div className={'padding'} /> */}
             {showDice && (
-                <RollingCubeAnimation desiredCubeProps={desiredCubeProps} />
+                <div>
+                    <RollingCubeAnimation desiredCubeProps={desiredCubeProps} />
+                    <RollingCubeAnimation desiredCubeProps={secondDesiredCubeProps} />
+                </div>
             )}
         </div>
         </>
